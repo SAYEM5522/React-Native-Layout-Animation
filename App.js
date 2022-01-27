1db816a0c0ae7be2cd90ea5ccd3ad753a5322ee1
SAYEM5522 <md1040582@gmail.com>
2022-01-27 19:10:28 +0600

    Graph Add

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import Animated, { interpolate, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import { LineChart } from 'react-native-wagmi-charts';
+const {width: SIZE} = Dimensions.get('window');
+const data = [
+  {
+    timestamp: 1625945400000,
+    value: 33575.25,
+  },
+  {
+    timestamp: 162594630000000,
+    value: 3354555.25,
+  },
+  {
+    timestamp: 1625947200000,
+    value: 335106.25,
+  },
+  {
+    timestamp: 1625948100000,
+    value: 3321566.25,
+  },
+  {
+    timestamp: 1625947200000,
+    value: 335106.25,
+  },
+  {
+    timestamp: 1625948100000,
+    value: 3321566.25,
+  },
+  {
+    timestamp: 1625947200000,
+    value: 335106.25,
+  },
+  {
+    timestamp: 1625948100000,
+    value: 3321566.25,
+  },
+  {
+    timestamp: 1625947200000,
+    value: 335106.25,
+  },
+  {
+    timestamp: 1625948100000,
+    value: 3321566.25,
+  },
+];
+
+
 
 export default function App() {
+  
+  const AnimatedWidth=useSharedValue(0);
+  useEffect(()=>{
+    AnimatedWidth.value=1;
+  },[AnimatedWidth.value])
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return{
+        width:withTiming(interpolate(AnimatedWidth.value,[0,1],[0,SIZE/3]),{duration:1000}),
+    }
+  })
+    
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Animated.View style={[styles.container]}>
+    <LineChart.Provider data={data}>
+          <LineChart
+           width={SIZE/3}
+           height={SIZE/4}
+          >
+            <LineChart.Path  /> 
+          </LineChart>
+      </LineChart.Provider>
+  </Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+   
+   }
 });
